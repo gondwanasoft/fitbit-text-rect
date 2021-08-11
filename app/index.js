dab5b3d1ba3d46fce474ed24b1a6c2de027a5a8d
Gondwana Software <gondwanasoft@users.noreply.github.com>
2021-08-12 08:35:48 +1000

    Use CSS

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ import document from 'document'
 import './widgets/text_rect'
 
 const myTextRectEl = document.getElementById('myTextRect')
-myTextRectEl.text = "My Cat"
+console.log(`app/index.js ${myTextRectEl.getElementById('text').text}`)
+myTextRectEl.getElementById('text').textLength = 32
+myTextRectEl.text = "My Cat's name is Faure"
--- app/widgets/construct-widgets.js
+++ app/widgets/construct-widgets.js
@@ -1,15 +1,17 @@
 import document from 'document';
 
 export const constructWidgets = (widgetType, construct) => {
-  // TODO 3 consider automatically constructing all widgets by default; don't do so if a particular flag is set
-  // TODO 3 it's inefficient to search all of document; could there be a better way? manually-callable function to construct, that takes an arg? className in elements that contain widgets?
+  // widgetType: string name: value of SVG element 'type' attribute' declared in widget <symbol>
+  // TODO 3 may not need 'construct' arg: all widgets must provide it.
   const widgets = document.getElementsByTypeName(widgetType);
   widgets.forEach(widget => {
-    const classes = widget.class.split(' ');  // array of all class names
-    if (classes.indexOf('widget-auto') >= 0) {
-      widget.class = widget.class;    // bring forward (ie, trigger) application of CSS styles
-      construct(widget);
-      // TODO 3 distinguish between 'construct' (add members to el) and 'draw' (lay out visible elements)
+    if (widget.id !== widget.type) {    // needed because old firmware will find the <use> AND the <symbol> for each widget instance
+      const classes = widget.class.split(' ');  // array of all class names
+      if (classes.indexOf('widget-auto') >= 0) {
+        widget.class = widget.class;    // bring forward (ie, trigger) application of CSS styles
+        construct(widget);
+        // TODO 3 distinguish between 'construct' (add members to el) and 'draw' (lay out visible elements)
+      }
     }
   });
 }
