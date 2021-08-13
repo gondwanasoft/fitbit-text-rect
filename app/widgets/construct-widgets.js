0630d5ac9592edce3f3d5a6e8db233eb8b97a3d7
Gondwana Software <gondwanasoft@users.noreply.github.com>
2021-08-14 08:39:09 +1000

    constructWidgets can search any ElementSearch

--- app/widgets/construct-widgets.js
+++ app/widgets/construct-widgets.js
@@ -1,13 +1,13 @@
 import document from 'document';
 
-export const constructWidgets = (widgetType, construct) => {
-  // widgetType: string name: value of SVG element 'type' attribute' declared in widget <symbol>
-  // TODO 3.1 may not need 'construct' arg: all widgets must provide it.
-  const widgets = document.getElementsByTypeName(widgetType);
+export const constructWidgets = (widgetType, construct, parent = document) => {
+  // widgetType: string name: value of SVG element 'type' attribute' declared in widget <symbol>.
+  // construct: function to be called to convert a GraphicsElement to a widget of the relevant type.
+  // parent: ElementSearch to be searched for widgetType.
+  const widgets = parent.getElementsByTypeName(widgetType);
   widgets.forEach(widget => {
-    if (widget.id !== widget.type) {    // needed because old firmware will find the <use> AND the <symbol> for each widget instance
+    if (widget.id !== widget.type)     // needed because old firmware will find the <use> AND the <symbol> for each widget instance
       construct(widget);
       // TODO 3.5 distinguish between 'construct' (add members to el) and 'draw' (lay out visible elements); need 'autoDraw' in config.js?
-    }
   });
 }
