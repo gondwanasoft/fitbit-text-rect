b4afee0bb0cdd24a18d8f3819c046d8c4131b3d9
Gondwana Software <gondwanasoft@users.noreply.github.com>
2021-08-16 06:52:06 +1000

    Use constructWidgets() in app/index.js

--- README.md
+++ README.md
@@ -3,6 +3,22 @@ A simplistic widget for Fitbit OS that displays text in a rectangle. The rectang
 
 ![examples](screenshot.jpg#center)
 
+Construction
+-
+Widgets need to be constructed before they can be used and display correctly. There are three ways to do this:
+
+* Set `auto-construct` to `true` in `config.js`
+* Call `constructTextRect()` to construct a specific element
+* Call `constructWidgets()` to construct all widgets within a specific element
+
+Redraw
+-
+A widget will be automatically redrawn if its `.style.display` is not `none`. This applies when the widget is first constructed and also whenever a relevant attribute is changed (*eg*, `text`).
+
+For simple widgets, redrawing is not a problem. However, if your app has many complex widgets, and some of them are not always visible, unnecessary redrawing could impact on performance. To avoid this, set `.style.display` to `none`. When you need the widget to be visible, change `.style.display` to `inline` or `inherit`. Unfortunately, such a change won't trigger an immediate redraw; you must do this manually. You can do this by calling `redraw()` or by updating a widget attribute that results in a redraw.
+
+Caveats
+-
 This is largely untested. The widget is probably lacking default attributes and may not respond to CSS.
 
 Pull requests are welcome!
--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import document from 'document'
 //import './widgets/text_rect'                      // sufficient if using auto-construct
 import { constructTextRect } from './widgets/text_rect'   // needed if not using auto-construct
+import { constructWidgets } from './widgets/text_rect/construct-widgets';   // needed if not using auto-construct and constructing all widgets within a parent
 import * as config from './widgets/text_rect/config';
 
-if (!config.autoConstruct) {
-  // Construct widgets manually:
-  constructTextRect(document.getElementById('myTextRect'))
-  constructTextRect(document.getElementById('myTextRect2'))
+if (!config.autoConstruct) {  // Construct widgets manually
+  // Construct each widget individually:
+  // constructTextRect(document.getElementById('myTextRect'))
+  // constructTextRect(document.getElementById('myTextRect2'))
+
+  // Construct all widgets within a parent:
+  constructWidgets('text-rect', constructTextRect, document);
 }
 
 const myTextRectEl = document.getElementById('myTextRect')
@@ -14,4 +18,4 @@ console.log(`app/index.js ${myTextRectEl.getElementById('text').text}`)
 myTextRectEl.getElementById('text').textLength = 32
 myTextRectEl.text = "A Volkswagen."
 myTextRectEl.style.display = 'inherit';
-myTextRectEl.redraw();
+myTextRectEl.redraw();    // must call this after changing .style.display from 'none'
